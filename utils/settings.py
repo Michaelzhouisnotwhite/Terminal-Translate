1f8f37f8f370aa8ed9dd7b25ed6178cb6e716f46
Michaelzhouisnotwhite <zyqing601@163.com>
2022-05-10 22:58:23 +0800

    fix error of translate.py

--- utils/settings.py
+++ utils/settings.py
@@ -1,7 +1,7 @@
 import os
 
 APP_ID = '20210819000921635'
-APP_KEY = 'lang_tb'
+APP_KEY = '_RuALqBSiy2n3ri5WSjT'
 __ENDPOINT = 'http://api.fanyi.baidu.com'
 __PATH = '/api/trans/vip/translate'
 REQUEST_URL = __ENDPOINT + __PATH

e06ba938b74687b2447f071343511425d68535d3
Michaelzhouisnotwhite <zyqing601@163.com>
2022-05-10 22:14:53 +0800

    complete one by one command translator

--- translate.py
+++ translate.py
@@ -12,14 +12,21 @@ def main():
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('target_words', help="target translate words", type=str)
-    parser.add_argument('--lang', '-l', help="language you use", default="en", type=str)
-    parser.add_argument('--to_lang', help="translate to the lang", default='zh')
-    # parser.add_argument('--init', '-i', help="init terminal translator")
+    parser.add_argument('--lang', '-l', help="language you use", default="auto", type=str)
+    parser.add_argument('--to_lang', '-t', help="translate to the lang", default='zh')
     return parser.parse_args()
 
 
 def make_md5(s, encoding='utf-8'):
     return md5(s.encode(encoding)).hexdigest()
+
+
+# 检验是否含有中文字符
+def is_contains_chinese(strs):
+    for _char in strs:
+        if '\u4e00' <= _char <= '\u9fa5':
+            return True
+    return False
 
 
 def get_salt() -> str:
@@ -29,8 +36,13 @@ def get_salt() -> str:
 def translate():
     args = get_args()
     query = args.target_words
-    lang = args.lang
-    to = args.to_lang
+    if is_contains_chinese(query):
+        lang = 'zh'
+        to = 'en'
+
+    else:
+        lang = args.lang
+        to = args.to_lang
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     salt = get_salt()
@@ -48,15 +60,13 @@ def translate():
         res = requests.post(REQUEST_URL, params=params, headers=headers)
         rjson = res.json()
     except Exception as e:
-        print("\033[0;31;40mSomething has gone Wrong! Probably due to your NetWork Status.\033[0m")
+        print("\033[0;31;mSomething has gone Wrong! Probably due to your NetWork Status.\033[0m")
 
     try:
         from_lang = rjson['from']
         to_lang = rjson['to']
         trans = rjson["trans_result"][0]['dst']
-        print(f'From:\33[35m{from_lang}\33[0m')
-        print(f'To:\33[36m{to_lang}\33[0m')
-        print(f'Translation:\33[32m{trans}\33[0m')
+        print(f'\33[36m[{from_lang}-{to_lang}]\33[0m \33[32m{trans}\33[0m')
 
     except Exception:
         print('\033[0;31;40mSomething has gone Wrong! Please check your config file!\033[0m')
